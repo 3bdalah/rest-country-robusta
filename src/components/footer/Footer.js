f4382986d6c9b995371bfd83e841600950430419
3bdalah <3bdallah.dev@gmail.com>
2021-09-18 16:10:58 +0200

    show structure

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            footerr
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+const header = () => {
+    return (
+        <div>
+            footerr
+        </div>
+    )
+}
+
+export default header;
